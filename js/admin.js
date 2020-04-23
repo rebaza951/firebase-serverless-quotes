41761534816a980cc26ac689abee49df394dd77f
rebaza951 <carlos_hgr@hotmail.com>
2020-04-22 23:50:59 -0500

    add cloud functions

---
+++ js/admin.js
@@ -0,0 +1,8 @@
+adminForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const email = document.querySelector("#admin-email").value;
+
+  const addAdminRole = func.httpsCallable("addAdminRole");
+  addAdminRole({ email }).then(console.log).catch(console.log);
+});
